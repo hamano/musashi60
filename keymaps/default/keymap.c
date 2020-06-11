5c5d332b0d95634323e9c08a00be38bfbe022a04
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-06-11 21:13:42 +0900

    add default keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -18,7 +18,8 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _RAISE,
+    _LOWER
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -27,18 +28,40 @@ enum custom_keycodes {
     QMKURL
 };
 
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+#define MT_CS MT(MOD_LCTL, KC_SPACE)
+#define MT_SS MT(MOD_LSFT, KC_SPACE)
+
+#define MT_LM MT(LOWER, KC_MHEN)
+#define MT_RH MT(RAISE, KC_HENK)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
-    )
+  [_BASE] = LAYOUT(
+            KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,        \
+    KC_LBRC,KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_RBRC,KC_BSPC,\
+    KC_2,   KC_MINS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,   KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, \
+    KC_3,   KC_EQL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,   KC_ENT, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_RSFT,\
+                                    KC_LALT,KC_LGUI,MT_CS,  MT_LM,    MT_RH,  MT_SS,  KC_HENK,KC_MHEN                                 \
+  ),
+  [_RAISE] = LAYOUT(
+            KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                    KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_NO,          \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_NO,  KC_NO,  KC_NO,  \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  \
+                                    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO                                   \
+  ),
+  [_LOWER] = LAYOUT(
+            KC_NO,  KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                   KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_NO,          \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  \
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  \
+                                    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO                                   \
+  )
 };
 
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case QMKBEST:
@@ -61,7 +84,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-/*
 void matrix_init_user(void) {
 
 }
