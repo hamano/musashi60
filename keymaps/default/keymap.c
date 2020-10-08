00f3cb52705512ddd56e556a6bd97b159343edb9
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-10-09 00:01:40 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -36,9 +36,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
                     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                   \
-    KC_APP, KC_Q,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_P,   KC_GRV, \
-    KC_HOME,KC_LBRC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,   KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,\
-    KC_END, KC_RBRC,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,   KC_ENT, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_MINS,KC_EQL, \
+    KC_GRV, KC_GRV, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,\
+    KC_HOME,KC_MINS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,   KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,\
+    KC_END, KC_EQL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,   KC_ENT, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_MINS,KC_EQL, \
     KC_LGUI,KC_LSFT,                MT_LALT,KC_LCTL,LOWER,  KC_SPACE,KC_SPACE,RAISE,  KC_RCTL,MT_RALT,                KC_RSFT,KC_RGUI \
   ),
   [_RAISE] = LAYOUT(
