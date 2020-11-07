4953dc37d666162f69d9be4597ba0fb86c500c6c
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-11-07 23:31:21 +0900

    update keylayout

--- musashi60.h
+++ musashi60.h
@@ -34,17 +34,17 @@
     LH2, LG2, LF2, LE2, LD2, LC2, LB2,           RB2, RC2, RD2, RE2, RF2, RG2, RH2, \
     LH3, LG3, LF3, LE3, LD3, LC3, LB3, LA3, RA3, RB3, RC3, RD3, RE3, RF3, RG3, RH3, \
     LH4, LG4, LF4, LE4, LD4, LC4, LB4, LA4, RA4, RB4, RC4, RD4, RE4, RF4, RG4, RH4, \
-    LH5, LG5,           LD5, LC5, LB5, LA5, RA5, RB5, RC5, RD5,           RG5, RH5  \
+         LG5,           LD5, LC5, LB5, LA5, RA5, RB5, RC5, RD5,           RG5       \
 ) \
 { \
     { XXX, LB1, LC1, LD1, LE1, LF1, XXX, XXX }, \
     { XXX, LB2, LC2, LD2, LE2, LF2, LG2, LH2 }, \
     { LA3, LB3, LC3, LD3, LE3, LF3, LG3, LH3 }, \
     { LA4, LB4, LC4, LD4, LE4, LF4, LG4, LH4 }, \
-    { LA5, LB5, LC5, LD5, XXX, XXX, LG5, LH5 },	\
+    { LA5, LB5, LC5, LD5, XXX, XXX, LG5, XXX },	\
     { XXX, RB1, RC1, RD1, RE1, RF1, XXX, XXX },	\
     { XXX, RB2, RC2, RD2, RE2, RF2, RG2, RH2 }, \
     { RA3, RB3, RC3, RD3, RE3, RF3, RG3, RH3 }, \
     { RA4, RB4, RC4, RD4, RE4, RF4, RG4, RH4 }, \
-    { RA5, RB5, RC5, RD5, XXX, XXX, RG5, RH5 }	\
+    { RA5, RB5, RC5, RD5, XXX, XXX, RG5, XXX }	\
 }
