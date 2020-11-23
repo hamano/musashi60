9dd842195f99195d3915efef80b307bf23ad3c4c
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-11-23 23:23:04 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -39,21 +39,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV, KC_EQL, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,\
     KC_BSLS,KC_MINS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,  KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_HOME,\
     KC_LGUI,MT_LALT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,  KC_ENT, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,MT_RALT,KC_RGUI,\
-            MT_LALT,                KC_LSFT,KC_LCTL,LOWER,  KC_SPC,  KC_SPC, RAISE,  KC_RCTL,KC_RSFT,                MT_RALT         \
+                                    KC_LSFT,KC_LCTL,LOWER,  KC_SPC,  KC_SPC, RAISE,  KC_RCTL,KC_RSFT,                                \
   ),
   [_RAISE] = LAYOUT(
                     KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                   KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,                 \
     KC_HOME,KC_PGUP,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,                 KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,_______,\
     KC_END, KC_PGDN,KC_MS_L,KC_MS_D,KC_MS_R,KC_NO,  KC_NO,  KC_APP,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_NO,  KC_NO,  KC_END, \
     _______,_______,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_HOME,KC_END, KC_PGUP,KC_PGDN,KC_NO,  _______,_______,\
-            _______,                _______,_______,_______,_______, _______,_______,_______,_______,                _______         \
+                                    _______,_______,_______,_______, _______,_______,_______,_______,                                \
   ),
   [_LOWER] = LAYOUT(
                     KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                  KC_F16, KC_F17, KC_F18, KC_F19, KC_F20,                 \
     KC_MUTE,KC_VOLU,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,_______,\
     _______,KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_R,_______,KC_ESC, KC_APP,  KC_NO,  KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,KC_NO,  KC_NO,  KC_HOME,\
     _______,_______,KC_NO,  KC_NO,  KC_NO,  _______,KC_TAB, _______, KC_NO,  KC_ENT, KC_BTN1,KC_BTN3,KC_BTN2,KC_NO,  KC_NO,  KC_END, \
-            _______,                        _______,_______,_______, _______,_______,_______,_______,_______,        _______         \
+                                    _______,_______,_______, _______,_______,_______,_______,_______,                                \
   )
 };
 
