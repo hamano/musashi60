fe95eec96d1437f7845b163b081e50b994d933d5
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-11-27 00:19:38 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
                     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                   \
     KC_GRV, KC_EQL, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,\
-    KC_BSLS,KC_MINS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,  KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_HOME,\
+    KC_NO,  KC_MINS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_ESC,  KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,\
     KC_LGUI,MT_LALT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,  KC_BSPC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,MT_RALT,KC_RGUI,\
                                     MT_LSFT,KC_LCTL,LOWER,  KC_SPC,  KC_ENT, RAISE,  KC_RCTL,MT_RSFT                                 \
   ),
