0132962c7f99629006f1148001900bca7707b893
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-12-16 01:28:09 +0900

    update for rev7 key layout

--- musashi60.h
+++ musashi60.h
@@ -30,21 +30,21 @@
 #define XXX KC_NO
 
 #define LAYOUT( \
-              LF1, LE1, LD1, LC1, LB1,           RB1, RC1, RD1, RE1, RF1,           \
-    LH2, LG2, LF2, LE2, LD2, LC2, LB2,           RB2, RC2, RD2, RE2, RF2, RG2, RH2, \
-    LH3, LG3, LF3, LE3, LD3, LC3, LB3, LA3, RA3, RB3, RC3, RD3, RE3, RF3, RG3, RH3, \
-    LH4, LG4, LF4, LE4, LD4, LC4, LB4, LA4, RA4, RB4, RC4, RD4, RE4, RF4, RG4, RH4, \
-                        LD5, LC5, LB5, LA5, RA5, RB5, RC5, RD5                      \
+              LE1, LD1, LC1, LB1, LA1,           RA1, RB1, RC1, RD1, RE1,           \
+    LG2, LF2, LE2, LD2, LC2, LB2, LA2,           RA2, RB2, RC2, RD2, RE2, RF2, RG2, \
+    LG3, LF3, LE3, LD3, LC3, LB3, LA3,           RA3, RB3, RC3, RD3, RE3, RF3, RG3, \
+              LE4, LD4, LC4, LB4, LA4, LA5, RA5, RA4, RB4, RC4, RD4, RE4,           \
+         LF5,           LE5, LD5, LC5, LB5, RB5, RC5, RD5, RE5,           RF5       \
 ) \
 { \
-    { XXX, LB1, LC1, LD1, LE1, LF1, XXX, XXX }, \
-    { XXX, LB2, LC2, LD2, LE2, LF2, LG2, LH2 }, \
-    { LA3, LB3, LC3, LD3, LE3, LF3, LG3, LH3 }, \
-    { LA4, LB4, LC4, LD4, LE4, LF4, LG4, LH4 }, \
-    { LA5, LB5, LC5, LD5, XXX, XXX, XXX, XXX },	\
-    { XXX, RB1, RC1, RD1, RE1, RF1, XXX, XXX },	\
-    { XXX, RB2, RC2, RD2, RE2, RF2, RG2, RH2 }, \
-    { RA3, RB3, RC3, RD3, RE3, RF3, RG3, RH3 }, \
-    { RA4, RB4, RC4, RD4, RE4, RF4, RG4, RH4 }, \
-    { RA5, RB5, RC5, RD5, XXX, XXX, XXX, XXX }	\
+    { LA1, LB1, LC1, LD1, LE1, XXX, XXX }, \
+    { LA2, LB2, LC2, LD2, LE2, LF2, LG2 }, \
+    { LA3, LB3, LC3, LD3, LE3, LF3, LG3 }, \
+    { LA4, LB4, LC4, LD4, LE4, XXX, XXX }, \
+    { LA5, LB5, LC5, LD5, LE5, LF5, XXX }, \
+    { RA1, RB1, RC1, RD1, RE1, XXX, XXX }, \
+    { RA2, RB2, RC2, RD2, RE2, RF2, RG2 }, \
+    { RA3, RB3, RC3, RD3, RE3, RF3, RG3 }, \
+    { RA4, RB4, RC4, RD4, RE4, XXX, XXX }, \
+    { RA5, RB5, RC5, RD5, RE5, RF5, XXX }  \
 }
