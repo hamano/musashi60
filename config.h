8809375560d33767ed487050f6462ea489b4a869
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-12-27 15:32:34 +0900

    change TAPPING_TERM

--- config.h
+++ config.h
@@ -251,4 +251,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
-#define TAPPING_TERM 300
+#define TAPPING_TERM 180
