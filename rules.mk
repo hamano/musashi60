307bace9aa350e04aeb9d74279aa8af0d06ad4f9
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-01-08 13:01:56 +0900

    update rules.mk

--- rules.mk
+++ rules.mk
@@ -31,3 +31,5 @@ AUDIO_ENABLE = no           # Audio output
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 SPLIT_KEYBOARD = yes
+POINTING_DEVICE_ENABLE = yes
+SRC += analog.c
