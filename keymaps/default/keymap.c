d717272add7cfa948cc5d280dda966ef28d62f40
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-01-24 22:04:47 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -29,16 +29,16 @@ enum custom_keycodes {
 };
 
 #define ALT_GRV LALT(KC_GRV)
-#define RAISE LT(_RAISE, KC_BSPC)     // Tap for BackSpace, hold for Raise
-#define LOWER LT(_LOWER, KC_ESC)      // Tap for TAB, hold for Lower
+#define RAISE LT(_RAISE, KC_SPC)     // Tap for BackSpace, hold for Raise
+#define LOWER LT(_LOWER, KC_SPC)      // Tap for TAB, hold for Lower
 #define MT_LGUI MT(MOD_LGUI, KC_QUOT) // Tap for MUHENKAN, hold for LGUI
 #define MT_RGUI MT(MOD_RGUI, KC_MINS) // Tap for HENKAN, hold for RGUI
 #define MT_LALT MT(MOD_LALT, KC_MHEN) // Tap for MUHENKAN, hold for LALT
 #define MT_RALT MT(MOD_RALT, KC_HENK) // Tap for HENKAN, hold for RALT
 #define MT_LCTL MT(MOD_LCTL, KC_TAB)  // Tap for Tab, hold for LCTL
 #define MT_RCTL MT(MOD_RCTL, KC_ENT)  // Tap for Enter, hold for RCTL
-#define MT_LSFT MT(MOD_LSFT, KC_SPC) // Tap for Space, hold for LSFT
-#define MT_RSFT MT(MOD_RSFT, KC_SPC) // Tap for BackSpace, hold for RSFT
+#define MT_LSFT MT(MOD_LSFT, KC_SPC)  // Tap for Space, hold for LSFT
+#define MT_RSFT MT(MOD_RSFT, KC_BSPC) // Tap for BackSpace, hold for RSFT
 #define CT_PGUP LCTL(KC_PGUP) // Ctrl + PageUp
 #define CT_PGDN LCTL(KC_PGDN) // Ctrl + PageDown
 
@@ -53,16 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT(
             KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,         \
     KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,CT_PGUP,KC_PGDN,KC_PGUP,KC_LBRC,KC_RBRC,KC_NO,  \
-    KC_PIPE,KC_MS_L,KC_MS_D,KC_MS_R,KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LCBR,KC_PIPE,\
-    KC_BSLS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RCBR,KC_BSLS,\
+    KC_PIPE,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LCBR,KC_PIPE,\
+    KC_BSLS,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2,KC_NO,  KC_NO,  CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RCBR,KC_BSLS,\
                     _______,_______,KC_TAB, _______,_______,_______,_______,_______                 \
   ),
   [_LOWER] = LAYOUT(
             KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
     KC_APP, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   CT_PGUP,KC_PGDN,KC_PGUP,KC_LPRN,KC_RPRN,KC_NO,  \
-    KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_R,_______,KC_ESC, KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,KC_LBRC,KC_TILD,\
+    KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_R,_______,KC_ESC, _______,_______,_______,_______,KC_LBRC,KC_TILD,\
     KC_NO  ,KC_NO,  KC_NO,  KC_NO,  _______,KC_TAB, KC_ENT, KC_BTN1,KC_BTN3,KC_BTN2,KC_RBRC,KC_GRV, \
-                    _______,_______,_______,_______,_______,KC_ENT, _______,_______                 \
+                    _______,_______,_______,_______,_______,_______,_______,_______	                \
   )
 };
 
