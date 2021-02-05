2673c3c970f8582c26e6cfca52838564e00f7ff3
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-02-06 00:04:47 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -45,23 +45,23 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
             KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,           \
-    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_EQL, \
-    KC_GRV, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_MINS,\
-    KC_LGUI,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_QUOT,\
+    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_GRV, \
+    KC_QUOT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_MINS,\
+    KC_LGUI,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL, \
     	            MT_LALT,MT_LSFT,MT_LCTL,LOWER,  RAISE,  MT_RCTL,MT_RSFT,MT_RALT                 \
   ),
   [_RAISE] = LAYOUT(
             KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,         \
-    KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,CT_PGUP,KC_PGDN,KC_PGUP,KC_LBRC,KC_RBRC,KC_NO,  \
-    KC_PIPE,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LCBR,KC_PIPE,\
-    KC_BSLS,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2,KC_NO,  KC_NO,  CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RCBR,KC_BSLS,\
+    KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,CT_PGUP,KC_PGDN,KC_PGUP,KC_LBRC,KC_RBRC,KC_PIPE,\
+    KC_PIPE,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LBRC,KC_LCBR,\
+    KC_BSLS,KC_BTN1,KC_BTN3,KC_BTN2,KC_NO,  KC_NO,  CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RBRC,KC_RCBR,\
                     _______,_______,KC_TAB, _______,_______,_______,_______,_______                 \
   ),
   [_LOWER] = LAYOUT(
             KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
-    KC_APP, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   CT_PGUP,KC_PGDN,KC_PGUP,KC_LPRN,KC_RPRN,KC_NO,  \
-    KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_R,_______,KC_ESC, _______,_______,_______,_______,KC_LBRC,KC_TILD,\
-    KC_NO  ,KC_NO,  KC_NO,  KC_NO,  _______,KC_TAB, KC_ENT, KC_BTN1,KC_BTN3,KC_BTN2,KC_RBRC,KC_GRV, \
+    KC_APP, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   CT_PGUP,KC_PGDN,KC_PGUP,KC_LPRN,KC_RPRN,KC_BSLS,\
+    KC_VOLD,KC_MS_L,KC_MS_D,KC_MS_R,_______,KC_ESC, _______,_______,_______,_______,KC_LBRC,KC_LCBR,\
+    KC_NO  ,KC_NO,  KC_NO,  KC_NO,  _______,KC_TAB, KC_ENT, KC_BTN1,KC_BTN3,KC_BTN2,KC_RBRC,KC_RCBR,\
                     _______,_______,_______,_______,_______,_______,_______,_______	                \
   )
 };
