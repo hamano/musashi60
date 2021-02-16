1706ffc0c41a2527a90788d0b462b04afe06bf21
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-02-16 17:03:44 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -28,16 +28,16 @@ enum custom_keycodes {
     QMKURL
 };
 
-#define RAISE LT(_RAISE, KC_SPC)      // Tap for Space, hold for Raise
-#define LOWER LT(_LOWER, KC_SPC)      // Tap for Space, hold for Lower
+#define RAISE LT(_RAISE, KC_BSPC)     // Tap for Space, hold for Raise
+#define LOWER LT(_LOWER, KC_BSPC)     // Tap for Space, hold for Lower
 #define MT_LGUI MT(MOD_LGUI, KC_QUOT) // Tap for MUHENKAN, hold for LGUI
 #define MT_RGUI MT(MOD_RGUI, KC_MINS) // Tap for HENKAN, hold for RGUI
 #define MT_LALT MT(MOD_LALT, KC_MHEN) // Tap for MUHENKAN, hold for LALT
 #define MT_RALT MT(MOD_RALT, KC_HENK) // Tap for HENKAN, hold for RALT
 #define MT_LCTL MT(MOD_LCTL, KC_TAB)  // Tap for Tab, hold for LCTL
 #define MT_RCTL MT(MOD_RCTL, KC_ENT)  // Tap for Enter, hold for RCTL
-#define MT_LSFT MT(MOD_LSFT, KC_DEL)  // Tap for Space, hold for LSFT
-#define MT_RSFT MT(MOD_RSFT, KC_BSPC) // Tap for BackSpace, hold for RSFT
+#define MT_LSFT MT(MOD_LSFT, KC_SPC)  // Tap for Space, hold for LSFT
+#define MT_RSFT MT(MOD_RSFT, KC_SPC)  // Tap for BackSpace, hold for RSFT
 #define CT_PGUP LCTL(KC_PGUP) // Ctrl + PageUp
 #define CT_PGDN LCTL(KC_PGDN) // Ctrl + PageDown
 #define ALT_GRV LALT(KC_GRV)
@@ -45,9 +45,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
             KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,           \
-    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_GRV, \
-    KC_QUOT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_MINS,\
-    KC_LGUI,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL, \
+    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_EQL, \
+    KC_GRV, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_MINS,\
+    KC_LGUI,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_QUOT,\
     	            MT_LALT,MT_LSFT,MT_LCTL,LOWER,  RAISE,  MT_RCTL,MT_RSFT,MT_RALT                 \
   ),
   [_RAISE] = LAYOUT(
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
             KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
     KC_APP, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,\
-    _______,_______,_______,_______,_______,_______,CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_LBRC,KC_LCBR,\
+    KC_BSLS,_______,_______,_______,_______,_______,CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_LBRC,KC_LCBR,\
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_RBRC,KC_RCBR,\
                     _______,_______,_______,_______,_______,_______,_______,_______	                \
   )
