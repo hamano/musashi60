fd9d57d19cf19b3b675cd8be019061fda8f3f8b4
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-02-17 19:49:47 +0900

    update config.h

--- config.h
+++ config.h
@@ -251,4 +251,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
-#define TAPPING_TERM 170
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
