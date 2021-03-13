daf8056dc3e8fc4a9d38a4a5139a1133c9adbaba
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-03-14 00:52:29 +0900

    add joystick calibration key

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "musashi60.h"
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
@@ -24,7 +25,8 @@ enum layer_names {
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-    SMILE = SAFE_RANGE
+    SMILE = SAFE_RANGE,
+	JS_CAL // joystick calibration
 };
 
 #define RAISE LT(_RAISE, KC_BSPC)     // Tap for BackSpace, hold for Raise
@@ -56,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______,_______,_______,_______,_______,_______,_______,_______                 \
   ),
   [_LOWER] = LAYOUT(
-            KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
+            KC_F11, KC_F12, KC_F13, KC_F14, JS_CAL, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
     KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSLS,\
     KC_PLUS,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_INS, KC_PLUS,\
     KC_LCBR,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,_______,_______,_______,KC_DEL, KC_RCBR,\
@@ -69,8 +71,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SMILE:
         if (record->event.pressed) {
             SEND_STRING(":)");
-        } else {
-            // when keycode QMKBEST is released
+        }
+        break;
+    case JS_CAL:
+        if (record->event.pressed) {
+            musashi60_calibration();
         }
         break;
     }
