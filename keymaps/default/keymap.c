9cc47661cc2dd8ce6b4ba38479af20b75a3544e4
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-04-22 22:59:29 +0900

    update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -19,7 +19,6 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY,
-    _EUCALYN,
     _RAISE,
     _LOWER
 };
@@ -39,13 +38,6 @@ enum custom_keycodes {
 #define ALT_GRV LALT(KC_GRV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_EUCALYN] = LAYOUT(
-    KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL, \
-    KC_GRV, KC_Q,   KC_W,   KC_F,   KC_R,   KC_J,   KC_DOT, KC_M,   KC_D,   KC_Y,   KC_P,   KC_MINS,\
-    KC_GRV, KC_A,   KC_O,   KC_E,   KC_I,   KC_U,   KC_G,   KC_T,   KC_K,   KC_S,   KC_SCLN,KC_QUOT,\
-    KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_H,   KC_COMM,KC_L,   KC_SLSH,KC_RSFT,\
-    	    KC_LGUI,KC_LALT,KC_LCTL,KC_SPC, LOWER,  RAISE,  KC_ENT, KC_RCTL,KC_RALT,KC_RGUI         \
-  ),
   [_QWERTY] = LAYOUT(
     KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,\
     KC_GRV, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS, \
@@ -55,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_RAISE] = LAYOUT(
     KC_APP, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_NO,  \
-    KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL, \
+    KC_BSLS,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL, \
     KC_PIPE,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LBRC,KC_RBRC,\
     KC_LBRC,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_END, KC_RBRC,\
             KC_NO,  _______,_______,_______,_______,_______,_______,_______,_______,KC_NO           \
   ),
   [_LOWER] = LAYOUT(
     KC_APP, KC_F11, KC_F12, KC_F13, KC_F14, JS_CAL, KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,  KC_NO,  \
-    KC_APP, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,\
+    KC_PIPE,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,\
     KC_LGUI,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   CT_PGUP,KC_PGDN,KC_UP,  KC_PGUP,KC_LCBR,KC_RCBR,\
     KC_LCBR,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BTN1,KC_LEFT,KC_DOWN,KC_RGHT,KC_DEL, KC_RCBR,\
             KC_NO,  _______,_______,_______,_______,_______,_______,_______,_______,KC_NO           \
