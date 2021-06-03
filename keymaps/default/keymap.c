a518875996dbe8a61bde94051b1226cf5f6ad5ec
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-06-03 23:13:00 +0900

    update key layout

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "musashi60.h"
+//#include "musashi60.h"
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
@@ -39,22 +39,22 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
-            KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,           \
+    KC_NO,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,  \
     KC_GRV, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC, KC_DEL, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,\
     KC_LGUI,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_TAB, KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,\
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,\
     	                    MT_LALT,KC_LCTL,KC_SPC, LOWER,  RAISE,  KC_ENT, KC_RCTL,MT_RALT                         \
   ),
   [_RAISE] = LAYOUT(
-            KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,         \
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_NO,  \
     KC_PIPE,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_APP, KC_HOME,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL, \
     _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_MENU,KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LBRC,KC_BSLS,\
     _______,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                   CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RBRC,_______,\
                             _______,_______,_______,_______,_______,_______,_______,_______                         \
   ),
   [_LOWER] = LAYOUT(
-            KC_F11, KC_F12, KC_F13, KC_F14, JS_CAL,                 KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,          \
-    KC_BSLS,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_APP, _______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,\
+    KC_NO,  KC_F11, KC_F12, KC_F13, KC_F14, JS_CAL,                 KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,  KC_NO,  \
+    KC_BSLS,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_APP, _______,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_PLUS,\
     _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   _______,_______,CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_LCBR,KC_PIPE,\
     _______,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,                   KC_BTN1,KC_HOME,KC_END, KC_RGHT,KC_RCBR,_______,\
                             _______,_______,_______,_______,_______,_______,_______,_______                         \
