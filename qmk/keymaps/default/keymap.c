c3c8915d90a54efee27ba93120a69b3bd7b1ef41
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-11-18 13:45:23 +0900

    update keymap

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -46,15 +46,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_RAISE] = LAYOUT(
     KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_NO,  \
-    KC_PIPE,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_DEL, KC_HOME,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,\
-    _______,_______,_______,_______,_______,_______,KC_INS, KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LBRC,KC_BSLS,\
+    KC_PIPE,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_INS, KC_HOME,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,\
+    _______,_______,_______,_______,_______,_______,KC_DEL, KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LBRC,KC_BSLS,\
     _______,_______,_______,_______,_______,_______,                CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RBRC,_______,\
                             _______,_______,_______,_______,_______,_______,_______,_______                         \
   ),
   [_LOWER] = LAYOUT(
     KC_NO,  KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                 KC_VOLU,KC_VOLD,KC_MUTE,DEBUG,  RESET,  KC_NO,  \
-    KC_BSLS,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_DEL, KC_HOME,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL, \
-    _______,_______,_______,_______,_______,_______,KC_INS, KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LCBR,KC_PIPE,\
+    KC_BSLS,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_INS, KC_HOME,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL, \
+    _______,_______,_______,_______,_______,_______,KC_DEL, KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_LCBR,KC_PIPE,\
     _______,_______,_______,_______,_______,_______,                CT_PGUP,KC_PGDN,KC_PGUP,CT_PGDN,KC_RCBR,_______,\
                             _______,_______,_______,_______,_______,_______,_______,_______                         \
   )
