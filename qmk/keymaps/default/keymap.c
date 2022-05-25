22a8cacfdf6cdacd1eabed3916ac23bf61590951
HAMANO Tsukasa <hamano@osstech.co.jp>
2022-05-26 08:57:02 +0900

    update keymap

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -31,8 +31,8 @@ enum custom_keycodes {
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
-#define MT_LCTL MT(MOD_LCTL, KC_MHEN) // Tap for MUHENKAN, hold for LCTL
-#define MT_RCTL MT(MOD_RCTL, KC_HENK) // Tap for HENKAN, hold for RCTL
+#define MT_LALT MT(MOD_LALT, KC_MHEN) // Tap for MUHENKAN, hold for LCTL
+#define MT_RALT MT(MOD_RALT, KC_HENK) // Tap for HENKAN, hold for RCTL
 #define ALT_GRV  LALT(KC_GRV) // Alt + GRV
 #define CT_PGUP LCTL(KC_PGUP) // Ctrl + PageUp
 #define CT_PGDN LCTL(KC_PGDN) // Ctrl + PageDown
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC, KC_DEL, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,\
     KC_LGUI,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_TAB, KC_BSPC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,\
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,\
-                            KC_NO,  MT_LCTL,KC_SPC, KC_LALT,RAISE,  KC_ENT, MT_RCTL,KC_NO                           \
+                            MT_LALT,KC_LCTL,KC_SPC, LOWER,  RAISE,  KC_ENT, KC_RCTL,MT_RALT                         \
   ),
   [_RAISE] = LAYOUT(
     KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_NO,  \
